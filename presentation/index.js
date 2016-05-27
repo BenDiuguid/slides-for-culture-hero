dc4fb519505f6ee3bb5eb84964fb44a3aef0c925
Ben Diuguid <bendiuguid@gmail.com>
2016-05-26 23:28:58 -0400

    Added slides 👍

--- presentation/index.js
+++ presentation/index.js
@@ -54,16 +54,21 @@ export default class Presentation extends React.Component {
     return (
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500} progress="pacman">
+
           <Slide bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Culture Hero
+            <Heading size={1} fit caps lineHeight={1} textColor="black">Culture Hero</Heading>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} textColor="black">
+              Culture Hero is an interactive company engagement platform that motivates and aligns your workforce with the science of gamification.
             </Heading>
           </Slide>
+
           <Slide bgColor="black">
-            <Heading size={1}>
-              Definitions
-            </Heading>
+            <Heading size={1}>Let's start with some definitions</Heading>
           </Slide>
+
           <Slide transition={["slide"]} bgColor="black">
             <Heading size={1}>
               Culture
@@ -74,6 +79,7 @@ export default class Presentation extends React.Component {
               <Cite>Chris</Cite>
             </BlockQuote>
           </Slide>
+
           <Slide transition={["slide"]} bgColor="black">
             <Heading size={1}>
               Engagement
@@ -83,17 +89,112 @@ export default class Presentation extends React.Component {
               <Cite>Chris</Cite>
             </BlockQuote>
           </Slide>
-          
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+
+          <Slide bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1} textColor="black">Features</Heading>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Sentiment analysis</Heading>
             <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
+              <Appear><ListItem>Channels like Slack, Lync, Emails</ListItem></Appear>
+              <Appear><ListItem>Vertical/Horizontal integrations.</ListItem></Appear>
             </List>
           </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Communication</Heading>
+            <List>
+              <Appear><ListItem>Manager (team leader) reports</ListItem></Appear>
+              <Appear><ListItem>"Fireside chats" allowing executives to 'pulse' videos to the company</ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Culture Profile Comparisons</Heading>
+            <List>
+              <Appear><ListItem>Employee-to-Employee</ListItem></Appear>
+              <Appear><ListItem>Team-to-Team</ListItem></Appear>
+              <Appear><ListItem>Company-to-Company - MNA</ListItem></Appear>
+            </List>
+          </Slide>
+
+
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Learning Management System</Heading>
+            <List>
+              <Appear><ListItem>Training Videos</ListItem></Appear>
+              <Appear><ListItem>Quizzes</ListItem></Appear>
+              <Appear><ListItem>Other content</ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1} textColor="black">CulturePoints&trade;</Heading>
+            <Appear>
+              <Heading size={6} textColor="black">CulturePoints&trade; gamify a system in which a company can increase their desired culture.</Heading>
+            </Appear>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1} textColor="black">How to gain CulturePoints&trade;</Heading>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Peer-to-Peer Recognition</Heading>
+            <List>
+              <Appear><ListItem>CulturePoint 'allowance' for employees to give away</ListItem></Appear>
+              <Appear><ListItem>Allowance is reset weekly to encourage members to look for more opportunities to share</ListItem></Appear>
+              <Appear><ListItem>Points for referals who get hired</ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Social Media</Heading>
+            <List>
+              <Appear><ListItem>Simply sharing to various media outlets like Twitter, Facebook, LinkedIn, Instagram with a given # can earn CulturePoints</ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Gamified Leadership Hierarchy</Heading>
+            <List>
+              <Appear><ListItem></ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Camera Quests</Heading>
+            <List>
+              <Appear><ListItem></ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Bounty Board</Heading>
+            <List>
+              <Appear><ListItem></ListItem></Appear>
+            </List>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={4} fit textColor="black">Potential CulturePoints&trade; Rewards</Heading>
+            <List>
+              <ListItem>Company Swag</ListItem>
+              <ListItem>Monetary stuff like gift cards</ListItem>
+              <ListItem>Extra vacation time</ListItem>
+              <ListItem>Cool vacation experiences</ListItem>
+              <ListItem>Work from the CEO's desk for a day</ListItem>
+              <ListItem>Go to a Conference, expenses paid</ListItem>
+              <ListItem>Avatar Upgrades! 👍😀🙌</ListItem>
+            </List>
+          </Slide>
+
+          <Slide bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1} textColor="black">Any Questions?</Heading>
+          </Slide>
+
         </Deck>
       </Spectacle>
     );
